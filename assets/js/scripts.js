9ba3f6647c06bf3a087de4051785172201ec52ed
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-02-28 14:00:05 -0500

    changes: no paralax, no gradient on top, no skills bar, adds links to name of project, change hover top buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,25 @@
+$(document).ready(function(){
+  init();
+})
+
+function init(){
+  // gen_mail_to_link();
+  scroll_nav();
+}
 function gen_mail_to_link(lhs,rhs,subject) {
   $('.mail').prepend("<a class='btn btn-social btn-envelope btn-simple' href=\"mailto:" + lhs + "@"+rhs + "?subject=" + subject + "\">" + "<i class='fa fa-envelope'>"+"<\/i>" + "<\/a>")
     }
+function scroll_nav(){
+  // $('a[href^="#"]').on('click', function(event) {
+  //   console.log("here");
+  //   console.log(this);
+  //     var target = $(this).attr('href');
+  //     console.log(target);
+  //     if( target.length ) {
+  //         event.preventDefault();
+  //         $('html, body').animate({
+  //             scrollTop: target.offset().top
+  //         }, 1000);
+  //     }
+  // });
+}

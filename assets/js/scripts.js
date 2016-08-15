4e94fcbecf44ce6912557023cdecb580d29f38a2
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-08-14 20:47:26 -0400

    adds resume page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,27 +3,11 @@ $(document).ready(function(){
 })
 
 function init(){
-  // console.log("mew");
-  //   imageWidth = $('.shadow-white img').width();
-  //   parentWidth = $('.shadow-white').width();
-  //   if (imageWidth > parentWidth) {
-  //       $('.shadow-white img').css('width', '100%');
-  //   }
 }
 function gen_mail_to_link(lhs,rhs,subject) {
   $('.mail').prepend("<a class='btn btn-social btn-envelope btn-simple' href=\"mailto:" + lhs + "@"+rhs + "?subject=" + subject + "\">" + "<i class='fa fa-envelope'>"+"<\/i>" + "<\/a>")
-    }
-// function scroll_nav(){
-  // $('a[href^="#"]').on('click', function(event) {
-  //   console.log("here");
-  //   console.log(this);
-  //     var target = $(this).attr('href');
-  //     console.log(target);
-  //     if( target.length ) {
-  //         event.preventDefault();
-  //         $('html, body').animate({
-  //             scrollTop: target.offset().top
-  //         }, 1000);
-  //     }
-  // });
-// }
+}
+
+function gen_mail_to_link_text(lhs,rhs,subject) {
+  $('.mail').prepend("<a href=\"mailto:" + lhs + "@"+rhs + "?subject=" + subject + "\">" + lhs + "@"+rhs + "<\/a>")
+}
